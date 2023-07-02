c65b3e23d618d8faa11656e911630017432bba13
shahhameem <me.habiba.ayob@gmail.com>
2023-07-02 15:56:31 +0530

    commit

--- run.sh
+++ run.sh
@@ -6,9 +6,9 @@ g++  "$1" -pedantic-errors
 # Check if the compilation was successful
 if [ $? -eq 0 ]; then
   # If compilation succeeded, run the program
-  ./a
+  ./a.out
   # Delete the executable
-  rm a
+  rm a.out
 else
   # If compilation failed, display an error message
   echo a
